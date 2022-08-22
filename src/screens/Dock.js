63af9546608f7810092e1f2d2d54aa3b18643702
ericmlantz <ericmlantz@gmail.com>
2022-08-21 21:57:45 -0400

    Test

--- src/screens/Dock.js
+++ src/screens/Dock.js
@@ -15,18 +15,21 @@ const Dock = () => {
         linktype="internal"
         page="/"
         width={dockitemwidth}
+        height="100%"
       />
       <PageLink
         src={require('../assets/projectIcon.png')}
         linktype="internal"
         page="/projects"
         width={dockitemwidth}
+        height="100%"
       />
       <PageLink
         src={require('../assets/resumeIcon.png')}
         linktype="internal"
         page="/resume"
         width={dockitemwidth}
+        height="100%"
       />
       <PageLink
         src={
@@ -35,28 +38,33 @@ const Dock = () => {
         linktype="external"
         page="https://www.linkedin.com/in/eric-lantz/"
         width={dockitemwidth}
+        height="100%"
       />
       <PageLink
         src={require('../assets/githubIcon.png')}
         linktype="external"
         page="https://github.com/ericmlantz"
         width={dockitemwidth}
+        height="100%"
       />
       <PageLink
         src={require('../assets/emailIcon.png')}
         linktype="external"
         page="mailto:ericmlantz@gmail.com"
         width={dockitemwidth}
+        height="100%"
       />
       {projectLocation.pathname === '/project/dinr' && (
         <PageLink
           width={dockitemwidth}
+          height="100%"
           src={require('../assets/dinrLogoIcon.png')}
         />
       )}
       {projectLocation.pathname === '/project/set' && (
         <PageLink
           width={dockitemwidth}
+          height="100%"
           src={require('../assets/setLogoIcon.png')}
         />
       )}
@@ -64,6 +72,7 @@ const Dock = () => {
         <PageLink
           className="pagelink"
           width={dockitemwidth}
+          height="100%"
           src={require('../assets/OutAndAboutIcon.png')}
         />
       )}

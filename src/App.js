1742f0c3cd3122dcb63505972cda51766a78432d
ericmlantz <ericmlantz@gmail.com>
2022-08-23 12:58:36 -0400

    Removed welcome screeen modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //imported from 3rd party libraries
 import { Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
 //imported from this repo
 import './styles/App.css'
 import Home from './screens/Home'
@@ -12,22 +11,10 @@ import OutAndAboutScreen from './screens/OutAndAboutScreen'
 
 //create the component
 export default function App() {
-  const [welcome, setWelcome] = useState()
-  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              setCount={setCount}
-              count={count}
-              setWelcome={setWelcome}
-              welcome={welcome}
-            />
-          }
-        />
+        <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/project/dinr" element={<DinrScreen />} />
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,26 +1,14 @@
 //imported from 3rd party libraries
-import { useEffect } from 'react'
+
 //imported from this repo
 import Dock from './Dock'
 import MenuBar from './MenuBar'
 import HomeMain from './HomeMain'
-import WelcomeScreen from './WelcomeScreen'
 //create the component
-const Home = ({ welcome, setWelcome, count, setCount }) => {
-  useEffect(() => {
-    if (count < 1) {
-      setWelcome(true)
-      setCount(1)
-    }
-  }, [])
-
-  const handleClick = () => {
-    setWelcome(false)
-  }
+const Home = () => {
   //render
   return (
     <>
-      {welcome === true && <WelcomeScreen handleClick={handleClick} />}
       <MenuBar title="Home" />
       <div className="main">
         <HomeMain />
